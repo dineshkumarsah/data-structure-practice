0a23459805db3af5d856bb495f296bac55801e9b
dinesh <dineshbhumca16@gmail.com>
2023-07-08 22:22:07 +0530

    ArithmaticException

---
+++ src/exceptionHandling/AgeException.java
@@ -0,0 +1,15 @@
+package exceptionHandling;
+
+public class AgeException {
+     void ageCheck(int num){
+        try{
+         int result = num/0; //java.lang.ArithmeticException: / by zero
+        }catch(Exception e){
+            System.out.println("Exception occuring");
+        }
+    }
+    public static void main(String[] args){
+       AgeException obj = new AgeException(); 
+       obj.ageCheck(12);
+    }
+}
